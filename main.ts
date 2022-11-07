4dd6ed3a5937cde28376d41d93142c13364d831e
Christian Mendez <112632376+Mendez002@users.noreply.github.com>
2022-11-07 11:08:28 -0300

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,21 @@
+input.onButtonPressed(Button.A, function () {
+    if (radio2 >= 255) {
+        radio2 = 255
+    } else {
+        radio2 += -1
+        radio.setGroup(radio2)
+        basic.showNumber(radio2)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (radio2 <= 0) {
+        radio2 = 0
+    } else {
+        radio2 += 1
+        radio.setGroup(radio2)
+        basic.showNumber(radio2)
+    }
+})
+let radio2 = 0
+radio2 = 1
+radio.setGroup(1)
